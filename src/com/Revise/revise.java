15c261d4867ce6e6336f8d261766f29993e99fa6
ABDULRUHMAN <abdsati6@icloud.com>
2023-08-11 01:53:33 +0300

    The new version

--- src/com/Revise/revise.java
+++ src/com/Revise/revise.java
@@ -11,28 +11,84 @@ public class revise {
 
         //*Collecting entering details*//
 
-        //Card Number
-        Scanner cardNumIn = new Scanner(System.in);
-        System.out.println("Your card number: ");
-        BigInteger cardNumberIn = cardNumIn.nextBigInteger();
-
-        //PinCode
-        Scanner PinCodeIn = new Scanner(System.in);
-        System.out.println("Your Pin code please: ");
-        int PinCodeIN = PinCodeIn.nextInt();
+        //*LogIn OR SignUp*//
+        Scanner User = new Scanner(System.in);
+        System.out.println("Do you want to Log in OR you want to Sign up? Reply 1 if you want to log in and 2 if you want to Sign up: ");
+        int userS = User.nextInt();
+        
+        if(userS == 1){
 
 
+            //Card Number
+            Scanner cardNumIn = new Scanner(System.in);
+            System.out.println("Your card number: ");
+            BigInteger cardNumberIn = cardNumIn.nextBigInteger();
 
-        //*The main process*//
+            //PinCode
+            Scanner PinCodeIn = new Scanner(System.in);
+            System.out.println("Your Pin code please: ");
+            int PinCodeIN = PinCodeIn.nextInt();
 
-        if(cardNumber.equals(cardNumberIn) && PinCode==PinCodeIN){
-            System.out.println("Welcome!! your net balance is: " + Balance + " Qatari riyals");
-        } else if (!(cardNumber.equals(cardNumberIn)) && PinCode==PinCodeIN) {
-            System.out.println("There is no account for this card number please double check the number you entered :( ");
-        } else if (cardNumber.equals(cardNumberIn) && !(PinCode==PinCodeIN)) {
-            System.out.println("You have entered wrong pin!! please try again...");
+
+
+            //*The main process*//
+
+            if(cardNumber.equals(cardNumberIn) && PinCode==PinCodeIN){
+                System.out.println("Welcome!! your net balance is: " + Balance + " Qatari riyals");
+            } else if (!(cardNumber.equals(cardNumberIn)) && PinCode==PinCodeIN) {
+                System.out.println("There is no account for this card number please double check the number you entered :( ");
+            } else if (cardNumber.equals(cardNumberIn) && !(PinCode==PinCodeIN)) {
+                System.out.println("You have entered wrong pin!! please try again...");
+            }
+
+
+        } else {
+            
+            //*New Account*//
+            Scanner NewCardNumIN = new Scanner(System.in);
+            System.out.println("Your card number: ");
+            BigInteger NewCardNum = NewCardNumIN.nextBigInteger();
+            
+            //*New Pin Code*//            
+            Scanner NewPinCodeIn = new Scanner(System.in);
+            System.out.println("Your pin code: ");
+            int NewPinCode = NewPinCodeIn.nextInt();
+            
+            //*Collecting Balance*//
+            Scanner NewBalanceIn = new Scanner(System.in);
+            System.out.println("Your Balance: ");
+            int NewBalance = NewBalanceIn.nextInt();
+            
+            
+            
+            
+            //*Logging in after Signing up*//
+            System.out.println("Congratulation!! you have Signed up now you can log in to your account");
+
+            //New card number
+            Scanner NewCardLoggingScan = new Scanner(System.in);
+            System.out.println("Your card number: ");
+            BigInteger NewCardLogging = NewCardLoggingScan.nextBigInteger();
+            
+            //New pin code
+            Scanner NewPinCodeScan = new Scanner(System.in);
+            System.out.println("Your Pin Code: ");
+            int NewPinCodeLogging = NewPinCodeScan.nextInt();
+            
+            //Logging process
+            
+            if(NewCardLogging.equals(NewCardNum) && NewPinCodeLogging == NewPinCode){
+                System.out.println("Welcome!! your Net Balance is: " + NewBalance + " Qatari riyals");
+            } else if (!(NewCardLogging.equals(NewCardNum))&& NewPinCodeLogging == NewPinCode) {
+                System.out.println("Your new card number is wrong!! please try again...");
+            } else if (NewCardLogging.equals(NewCardNum) && !(NewPinCodeLogging == NewPinCode)) {
+                System.out.println("Your new pin code is not correct :(. please try again...");
+            }
+
+
         }
-
+        
+       
 
     }
 }
